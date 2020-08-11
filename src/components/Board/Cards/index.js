cd01a6438a6f6b4e18d9eb474024d54c0966d78f
Melissa <melissakheying@gmail.com>
2020-08-11 13:29:18 -0500

    :construction: duplicate and concat cards from api

--- src/components/Board/Cards/index.js
+++ src/components/Board/Cards/index.js
@@ -1,34 +1,47 @@
-import React, {useEffect, useState, Fragment} from 'react'
+import React, { useEffect, useState, Fragment } from 'react'
 import PropTypes from 'prop-types'
+
 import api from "api"
+
 import './Cards.css'
-import {Card} from './Card'
 
-export const Cards = ({handler}) => {
+import { Card } from './Card'
+
+export const Cards = ({ handler }) => {
   const [cards, setCards] = useState([])
+  const [flippedCards, setFlippedCards] = useState([])
 
 
 
-useEffect(() => {
-  (async () => {
-    const {cards} = await api.index(4)
-    setCards(cards)
-  })()
-}, [])
+  useEffect(() => {
+    (async () => {
+      const { cards } = await api.index(4)
+      //duplicate cards
+      const dupeCards = cards.concat(Array.from(cards))
+      // assign id using code and index
+      setCards(dupeCards)
+    })()
+  }, [])
 
 
-const renderCards = () => {
- return cards.map(({image, value, suit}, i) => <Card image={image} value={value} suit={suit} key={i} flipHandler={flipHandler}/>
+  const renderCards = () => {
+    return cards.map(({ image, value, suit }, i) => (
+    <Card
+    image={image}
+    value={value}
+    suit={suit} key={i}
+    flipHandler={flipHandler}
+    />
+    ))
+  }
+
+  const flipHandler = (event) => {
+    console.log(event.target)
+  }
+
+  return (
+    <div className="container">{renderCards()}</div>
   )
-}
-
-const flipHandler = (event) => {
-  console.log(event.target)
-}
-
-return (
-  <div className="container">{renderCards()}</div>
-)
 }
 
 Cards.propTypes = {
