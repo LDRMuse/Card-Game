99bed2c628587f63fa3fb26fc27082e905d57e28
Melissa <melissakheying@gmail.com>
2020-08-11 14:18:25 -0500

    Place img in button to add onClick

--- src/components/Board/Cards/Card.js
+++ src/components/Board/Cards/Card.js
@@ -2,20 +2,20 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 
-export const Card = ({image, value, suit, flipped, matched, flipHandler, code, id}) => {
+export const Card = ({ image, value, suit, flipped, matched, flipHandler, code, id }) => {
 
 
 
-return (
-<img
-src={flipped ? image : "https://source.unsplash.com/random/226x314"}
-alt={`${value} of ${suit}`}
-onClick={flipHandler}
-data-code={code}
-data-id={id}
-/>
-
-)
+  return (
+    <button onClick={flipHandler}>
+      <img
+        src={flipped ? image : "https://source.unsplash.com/random/226x314"}
+        alt={`${value} of ${suit}`}
+        data-code={code}
+        data-id={id} //dataset.id
+      />
+    </button>
+  )
 
 }
 
