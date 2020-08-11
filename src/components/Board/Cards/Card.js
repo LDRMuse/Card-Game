599cdc400f4ce9ccc038f262f5dd6135b6696bef
Melissa <melissakheying@gmail.com>
2020-08-11 13:18:07 -0500

    :lipstick: use grid to format
    Add CSS to organize cards
    
    :lipstick: add width

--- src/components/Board/Cards/Card.js
+++ src/components/Board/Cards/Card.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types'
 export const Card = ({image, value, suit, flipped, matched, flipHandler}) => {
 
 
+
 return (
 <img src={flipped ? image : "https://source.unsplash.com/random/226x314"} alt={`${value} of ${suit}`} onClick={flipHandler}/>
 
