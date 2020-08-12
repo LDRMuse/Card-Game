7bea63398ab362c31508219940a073b12500e42f
Melissa <melissakheying@gmail.com>
2020-08-12 13:57:23 -0500

    Flip the cards when clicked

--- src/components/Board/Cards/index.js
+++ src/components/Board/Cards/index.js
@@ -9,6 +9,9 @@ import { Card } from './Card'
 export const Cards = ({ handler, cards }) => {
   // This will just manage flipped and matched cards
   const [flippedCards, setFlippedCards] = useState([])
+  const [matchedCards, setMatchedCards] = useState([])
+
+  // if matchedCards.length = cards.length / 2, then stop the timer
 
 
 
@@ -38,13 +41,14 @@ export const Cards = ({ handler, cards }) => {
 
 
   const flipHandler = ({ target: { dataset } }) => {
+
+    // if its TRUE that no cards are flipped
     if (!flippedCards.length) {
       setFlippedCards((flippedCards) =>
-        flippedCards.concat({
-          id: dataset.id,
-          code: dataset.code,
-        })
+        // id is for identifying EACH card, code is for comparing matching TWO cards
+        flippedCards.concat({ id: dataset.id, code: dataset.code, })
       )
+      // check to make sure same card was not clicked twice
     } else if (flippedCards[0].id !== dataset.id) {
       setFlippedCards((flippedCards) =>
         flippedCards.concat({
@@ -56,24 +60,30 @@ export const Cards = ({ handler, cards }) => {
   }
 
   const renderCards = () => {
-    return cards.map(({ code, id, image, value, suit, flipped, matched }, i) => (
-      <Card
-        code={code}
-        flipHandler={flipHandler}
-        flipped={flipped}
-        id={id}
-        image={image}
-        matched={matched}
-        suit={suit} key={i}
-        value={value}
+    return cards.map(({ code, id, image, value, suit, flipped, matched }, i) => {
+      // if current card is listed in flippedCards, set it as flipped before it gets rendered
+      if(id === flippedCards[0]?.id || id === flippedCards[1]?.id) {
+        flipped = true
+      }
+
+
+    return <Card
+      code = { code }
+      flipHandler = { flipHandler }
+      flipped = { flipped }
+      id = { id }
+      image = { image }
+      matched = { matched }
+      suit = { suit } key = { i }
+      value = { value }
       />
-    ))
-  }
+    })
+}
 
 
-  return (
-    <div className="container">{renderCards()}</div>
-  )
+return (
+  <div className="container">{renderCards()}</div>
+)
 }
 
 Cards.propTypes = {
