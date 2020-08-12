7931e83261c50df9dbe917a70c3de2c20dd7c9dd
Melissa <melissakheying@gmail.com>
2020-08-12 14:04:33 -0500

    Add flipped cards if they are not dupes and don't already have 2

--- src/components/Board/Cards/index.js
+++ src/components/Board/Cards/index.js
@@ -15,31 +15,6 @@ export const Cards = ({ handler, cards }) => {
 
 
 
-  // useEffect(() => {
-  //   setCards((prevCards) =>
-  //     prevCards.map((card) => {
-  //       if (card.id === (flippedCards[0]?.id || card.id === flippedCards[1]?.id)) {
-  //         card.flipped = true
-  //       }
-  //       return card
-  //     })
-  //   )
-
-
-  //   if (flippedCards[0]?.code === flippedCards[1]?.code) {
-  //     cards.map((card) => {
-  //       if (card.id === (flippedCards[0]?.id || card.id === flippedCards[1]?.id)) {
-  //         card.matched = true
-  //         return card
-  //       }
-
-  //     })
-  //     // setFlippedCards([])
-  //   }
-  // }, [flippedCards])
-
-
-
   const flipHandler = ({ target: { dataset } }) => {
 
     // if its TRUE that no cards are flipped
@@ -49,7 +24,7 @@ export const Cards = ({ handler, cards }) => {
         flippedCards.concat({ id: dataset.id, code: dataset.code, })
       )
       // check to make sure same card was not clicked twice
-    } else if (flippedCards[0].id !== dataset.id) {
+    } else if (flippedCards[0].id !== dataset.id && flippedCards.length < 2) {
       setFlippedCards((flippedCards) =>
         flippedCards.concat({
           id: dataset.id,
@@ -59,35 +34,35 @@ export const Cards = ({ handler, cards }) => {
     }
   }
 
-  const renderCards = () => {
-    return cards.map(({ code, id, image, value, suit, flipped, matched }, i) => {
-      // if current card is listed in flippedCards, set it as flipped before it gets rendered
-      if(id === flippedCards[0]?.id || id === flippedCards[1]?.id) {
-        flipped = true
-      }
+    const renderCards = () => {
+      return cards.map(({ code, id, image, value, suit, flipped, matched }, i) => {
+        // if current card is listed in flippedCards, set it as flipped before it gets rendered
+        if (id === flippedCards[0]?.id || id === flippedCards[1]?.id) {
+          flipped = true
+        }
 
 
-    return <Card
-      code = { code }
-      flipHandler = { flipHandler }
-      flipped = { flipped }
-      id = { id }
-      image = { image }
-      matched = { matched }
-      suit = { suit } key = { i }
-      value = { value }
-      />
-    })
-}
+        return <Card
+          code={code}
+          flipHandler={flipHandler}
+          flipped={flipped}
+          id={id}
+          image={image}
+          matched={matched}
+          suit={suit} key={i}
+          value={value}
+        />
+      })
+    }
 
 
-return (
-  <div className="container">{renderCards()}</div>
-)
-}
+    return (
+      <div className="container">{renderCards()}</div>
+    )
+  }
 
-Cards.propTypes = {
-  //start stop timer
-  cards: PropTypes.array.isRequired,
-  handler: PropTypes.func,
-}
+  Cards.propTypes = {
+    //start stop timer
+    cards: PropTypes.array.isRequired,
+    handler: PropTypes.func,
+  }
