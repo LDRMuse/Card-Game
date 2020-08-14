ea131f4cc01677dceabc4c9de216fde19f731000
Melissa <melissakheying@gmail.com>
2020-08-14 11:05:14 -0500

    :construction: Create cardPair state; dropdown form; useEffect 2 fetch

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,14 +1,44 @@
-import React, { useState } from 'react'
+import React, { useState, Fragment } from 'react'
 import { Cards } from './Cards'
 import { Timer } from './Timer'
-import { Pairs } from './Pairs'
+import api from 'api'
+
 
 
 
 export const Board = () => {
   const [toggleTimer, setToggleTimer] = useState(false)
-  // setting timer to false means timer off
+  const [cardPairs, setCardPairs] = useState([])
+
+
+  // create new state to add up to 4 pairs of cards
+
+// use useEffect to grab 16 cards, or 4 pairs of cards and set that to the cardPairs state
+
+useEffect(() => {
+  (async () => {
+    decks = await api.index(16)
+    setCardPairs(decks)
+  })()
+
+}, [cardPairs])
+
+
   // const [gameOver, setGameOver] = useState('')
+  // Add another component in the 'parent'('board' / 'concentration') such that we can specify how many card pairs we want.Then,
+  //   fetch
+  // appropriate number of cards.
+
+  //   It’s the same FETCH for the cards.‘Concentration’ game, but now user gets to pick how many cards they want.
+  //     So, u just need to get that info on a ‘form submission’ and parent will pass that down to ‘cards.’ Currently, we ✍️ 4 cards.We need to draw however many user asks for.
+
+
+
+
+
+
+
+
 
   const handleCards = (toggle) => {
     setToggleTimer(toggle)
@@ -16,10 +46,23 @@ export const Board = () => {
 
 
   return (
-    <main>
-      <Cards handler={handleCards} />
-      <Timer toggle={toggleTimer} />
-      <Pairs />
-    </main>
+    // create a drop down form to let user choose ho many pairs they want to play with
+    <Fragment>
+      <p>How many pairs would you like to play with?</p>
+      <form type="submit">
+        <label htmlFor="cards">Choose a pair amount:</label>
+        <select name="cards">
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+        </select>
+        <input type="submit" value="Submit"/>
+        </form>
+
+        <Cards handler={handleCards} />
+        <Timer toggle={toggleTimer} />
+
+    </Fragment>
   )
 }
