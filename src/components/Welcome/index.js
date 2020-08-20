c3543828ba1dd0b36f62d93ea314717213e12139
Melissa <melissakheying@gmail.com>
2020-08-20 14:09:24 -0500

    :recycle: refactor

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,23 +1,23 @@
 import React, { Fragment } from 'react'
-import {useHistory} from "react-router-dom"
+import { useHistory } from "react-router-dom"
 
 
 
 
 
 export const Welcome = () => {
-const history = useHistory()
+  const history = useHistory()
 
 
 
-const handleClick = () => {
-  history.push("/how-to-play")
-}
+  const handleClick = () => {
+    history.push("/how-to-play")
+  }
 
   return (
-   <Fragment>
+    <Fragment>
       <h1>Welcome to Card Matching Game!</h1>
       <button onClick={handleClick}>How to Play</button>
-   </Fragment>
+    </Fragment>
   )
 }
