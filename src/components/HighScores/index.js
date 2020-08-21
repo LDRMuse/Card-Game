dddae4f5bcf73684de0bb1c6dfe66cb03669ecc4
Melissa <melissakheying@gmail.com>
2020-08-21 10:47:58 -0500

    Pass {time} state to table

--- src/components/HighScores/index.js
+++ src/components/HighScores/index.js
@@ -1,42 +1,47 @@
-import React, {useState, Fragment} from 'react'
+import React, { useState, Fragment } from 'react'
+import PropTypes from 'prop-types'
 
-export const HighScores = () => {
-const [scores] = useState([
-  {
-    initials: 'MH',
-    time: '1:11',
-    numOfCards: 24,
-  }
-])
+export const HighScores = ({ time }) => {
+  const [scores] = useState([
+    {
+      initials: 'MH',
+    }
+  ])
 
 
-const renderTableRows = () =>
-  scores.map(({initials, time, numOfCards}, i) => {
-    return (
-      <tr key={i}>
-      <td>{initials}</td>
-      <td>{time}</td>
-      <td>{numOfCards}</td>
-      </tr>
-    )
-  })
+  const renderTableRows = () =>
+    scores.map(({ initials }, i) => {
+      return (
+        <tr key={i}>
+          <td>{initials}</td>
+        </tr>
+      )
+    })
 
 
 
 
-return (
-  <Fragment>
-<h2>High Scores!!</h2>
-  <table>
-  <tr>
-    <th>Initials</th>
-    <th>Time</th>
-    <th>Cards</th>
-  </tr>
-{renderTableRows()}
-</table>
-</Fragment>
+
+  return (
+    <Fragment>
+      <h2>High Scores!!</h2>
+      <table>
+        <tr>
+          <th>Initials</th>
+          <th>Time</th>
+        </tr>
+        <tr>
+          <td>{renderTableRows()}</td>
+          <td>{time}</td>
+        </tr>
+      </table>
+    </Fragment>
 
 
-)
+  )
+}
+
+
+HighScores.propTypes = {
+  time: PropTypes.number
 }
